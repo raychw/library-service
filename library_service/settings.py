3d1002a65215177a94fd8c8ad734c4ee0f8a1da6
raychw <bladeryz@gmail.com>
2024-11-20 21:08:08 +0200

    implemented the use of JWT token and updated requirements.txt

--- library_service/settings.py
+++ library_service/settings.py
@@ -104,6 +104,12 @@ AUTH_PASSWORD_VALIDATORS = [
 
 AUTH_USER_MODEL = "users.User"
 
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ]
+}
+
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
