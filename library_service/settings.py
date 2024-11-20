d55afa9bdefcc007ca49f0779aec48a77fba3914
raychw <bladeryz@gmail.com>
2024-11-20 20:59:24 +0200

    added user model with email field and registered it in admin.py

--- library_service/settings.py
+++ library_service/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "books",
+    "users",
 ]
 
 MIDDLEWARE = [
@@ -101,6 +102,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = "users.User"
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
